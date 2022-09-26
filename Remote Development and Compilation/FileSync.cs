91e24a9faff4e66c50355a3591cca0588f169f79
jishimwe <jeanpaulishimwe@gmail.com>
2022-09-26 22:04:17 +0200

    First commit

---
+++ Remote Development and Compilation/FileSync.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remote_Development_and_Compilation
+{
+    internal class FileSync
+    {
+        public static void Main()
+        {
+            string path = @"C:\Users\ishim\source\repos\Remote Development and Compilation\Test Files\TFE23-254-Raincode_DistributedDev.txt";
+
+            using (FileStream fs = File.OpenRead(path))
+            {
+                byte[] buffer = new byte[4096];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                while (fs.Read(buffer, 0, buffer.Length) > 0)
+                {
+                    Console.WriteLine(temp.GetString(buffer));
+                }
+            }
+        }
+    }
+}
